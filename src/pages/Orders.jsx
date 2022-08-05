605955fd8266215e41c4fa904afa878134dd5051
Hunter <88465412+Hunyeh@users.noreply.github.com>
2022-08-05 16:13:10 -0400

    added orders page with data

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {  GridComponent, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, ColumnDirective } from '@syncfusion/ej2-react-grids';
+import { ordersData, contextMenuItems, ordersGrid } from "../data/data/dummy";
+import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className='margin-2 md:margin-10 p-2 md:p-10 bg- white rounded-3xl'>
+      <Header category='Page' title='Orders' />
+      <GridComponent
+        id='gridcomp'
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+      </GridComponent>
+    </div>
   )
 }
 
