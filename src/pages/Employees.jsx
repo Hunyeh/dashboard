ed190aeafa2c3ee6b4a1b20d6925e428cb739a32
Hunyeh <doringhunter@yahoo.com>
2022-08-07 10:28:15 -0400

    added cutomers and employees page
    added

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {  GridComponent, ColumnsDirective, Page, Search, Inject, ColumnDirective, Toolbar } from '@syncfusion/ej2-react-grids';
+import { employeesData, contextMenuItems, employeesGrid } from "../data/data/dummy";
+import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='margin-2 md:margin-10 p-2 md:p-10 bg- white rounded-3xl'>
+      <Header category='Page' title='Employees' />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
