ad3fe0f5b327cac0a8d956c5664401f7d8e4aa75
Hunyeh <doringhunter@yahoo.com>
2022-09-12 22:45:28 -0400

    added some charts and fixed calendar some

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-24'>
+      <p className='dark:text-gray-200 text-gray-700 text-center m-20'> @ 2022 All rights reserved by Hunyeh</p>
+    </div>
   )
 }
 
